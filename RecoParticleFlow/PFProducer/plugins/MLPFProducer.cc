29eedd793813dbdf7f6d12de131cfa203843c7ea
Joosep Pata <joosep.pata@gmail.com>
2021-06-28 15:30:56 +0300

    move mlpf from TF to ONNX

--- RecoParticleFlow/PFProducer/plugins/MLPFProducer.cc
+++ RecoParticleFlow/PFProducer/plugins/MLPFProducer.cc
@@ -4,54 +4,33 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
-#include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
+#include "PhysicsTools/ONNXRuntime/interface/ONNXRuntime.h"
 #include "RecoParticleFlow/PFProducer/interface/MLPFModel.h"
 
 #include "DataFormats/ParticleFlowReco/interface/PFBlockElementTrack.h"
 
-const std::vector<float> multiclass_thresholds = {0.34338968, 0.44481727, 0.63065319, 0.49350484, 0.57190419,
-       0.21699044, 0.92387225, 0.58548798};
+using namespace cms::Ort;
 
-struct MLPFCache {
-  const tensorflow::GraphDef* graph_def;
-};
-
-class MLPFProducer : public edm::stream::EDProducer<edm::GlobalCache<MLPFCache> > {
+class MLPFProducer : public edm::stream::EDProducer<edm::GlobalCache<ONNXRuntime> > {
 public:
-  explicit MLPFProducer(const edm::ParameterSet&, const MLPFCache*);
+  explicit MLPFProducer(const edm::ParameterSet&, const ONNXRuntime*);
   void produce(edm::Event& event, const edm::EventSetup& setup) override;
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
   // static methods for handling the global cache
-  static std::unique_ptr<MLPFCache> initializeGlobalCache(const edm::ParameterSet&);
-  static void globalEndJob(MLPFCache*);
+  static std::unique_ptr<ONNXRuntime> initializeGlobalCache(const edm::ParameterSet&);
+  static void globalEndJob(const ONNXRuntime*);
 
 private:
   const edm::EDPutTokenT<reco::PFCandidateCollection> pfCandidatesPutToken_;
   const edm::EDGetTokenT<reco::PFBlockCollection> inputTagBlocks_;
-  const std::string model_path_;
-  tensorflow::Session* session_;
+  std::vector<double> multiclass_thresholds_;
 };
 
-MLPFProducer::MLPFProducer(const edm::ParameterSet& cfg, const MLPFCache* cache)
+MLPFProducer::MLPFProducer(const edm::ParameterSet& cfg, const ONNXRuntime* cache)
     : pfCandidatesPutToken_{produces<reco::PFCandidateCollection>()},
       inputTagBlocks_(consumes<reco::PFBlockCollection>(cfg.getParameter<edm::InputTag>("src"))),
-      model_path_(cfg.getParameter<std::string>("model_path")) {
-  session_ = tensorflow::createSession(cache->graph_def);
-}
-
-std::vector<float> softmax(std::vector<float> logits) {
-    std::vector<float> exps;
-    float norm = 0.0; 
-    for (auto v : logits) {
-        const auto ev = std::exp(v);
-        exps.push_back(ev);
-        norm += ev;
-    }
-    for (size_t i=0; i<exps.size(); i++) {
-        exps[i] /= norm;
-    }
-    return exps;
+      multiclass_thresholds_(cfg.getParameter<std::vector<double>>("multiclass_thresholds")) {
 }
 
 void MLPFProducer::produce(edm::Event& event, const edm::EventSetup& setup) {
@@ -76,15 +55,12 @@ void MLPFProducer::produce(edm::Event& event, const edm::EventSetup& setup) {
   //const auto tensor_size = 6400;
   assert(tensor_size <= NUM_MAX_ELEMENTS_BATCH);
   assert(tensor_size % LSH_BIN_SIZE == 0);
+
   std::cout << "tensor_size=" << tensor_size << std::endl;
 
-  //Create the input tensor
-  tensorflow::TensorShape shape({BATCH_SIZE, tensor_size, NUM_ELEMENT_FEATURES});
-  tensorflow::Tensor input(tensorflow::DT_FLOAT, shape);
-  input.flat<float>().setZero();
-
-  //Fill the input tensor
-  unsigned int ielem = 0;
+  //Fill the input tensor (batch, elems, features) = (1, tensor_size, NUM_ELEMENT_FEATURES)
+  std::vector<std::vector<float>> inputs(1, std::vector<float>(NUM_ELEMENT_FEATURES*tensor_size, 0.0));
+  unsigned int ielem = 0;  
   for (const auto* pelem : selected_elements) {
     if (ielem > tensor_size) {
       continue;
@@ -97,97 +73,74 @@ void MLPFProducer::produce(edm::Event& event, const edm::EventSetup& setup) {
 
     //copy features to the input array
     for (unsigned int iprop = 0; iprop < NUM_ELEMENT_FEATURES; iprop++) {
-      input.tensor<float, 3>()(0, ielem, iprop) = normalize(props[iprop]);
+      inputs[0][ielem*NUM_ELEMENT_FEATURES + iprop] = normalize(props[iprop]);
     }
     ielem += 1;
   }
-  std::cout << "ielem=" << ielem << std::endl;
-
-  //TF model input and output tensor names
-  const tensorflow::NamedTensorList input_list = {{"x:0", input}};
-  const std::vector<std::string> output_names = {"Identity:0"};
-
-  //Prepare the output tensor
-  std::vector<tensorflow::Tensor> outputs;
 
   //run the GNN inference, given the inputs and the output.
-  //Note that the GNN enables information transfer between the input PFElements,
-  //such that the output ML-PFCandidates are in general combinations of the input PFElements, in the form of
-  //y_out = Adj.x_in, where x_in is input matrix (num_elem, NUM_ELEMENT_FEATURES), y_out is the output matrix (num_elem, NUM_OUTPUT_FEATURES)
-  //and Adj is an adjacency matrix between the elements that is constructed on the fly during model inference.
-  tensorflow::run(session_, input_list, output_names, &outputs);
-
-  //process the output tensor to ML-PFCandidates.
-  //The output can contain up to num_elem particles, with predicted PDGID=0 corresponding to no particles predicted.
-  const auto out_arr = outputs[0].tensor<float, 3>();
+  const auto& outputs = globalCache()->run({"x:0"}, inputs, {{1, tensor_size, NUM_ELEMENT_FEATURES}});
+  const auto& output = outputs[0];
+  assert(output.size() == tensor_size*NUM_OUTPUT_FEATURES);
 
   std::vector<reco::PFCandidate> pOutputCandidateCollection;
-  for (unsigned int ielem = 0; ielem < selected_elements.size(); ielem++) {
-    //get the coefficients in the output corresponding to the class probabilities (raw logits)
-    std::vector<float> pred_id_probas;
-    for (unsigned int idx_id = 0; idx_id <= IDX_CLASS; idx_id++) {
-      auto pred_proba = out_arr(0, ielem, idx_id);
-      assert(!std::isnan(pred_proba));
-      pred_id_probas.push_back(pred_proba);
-    }
+  for (size_t ielem=0; ielem<num_elements_total; ielem++) {
+    std::vector<float> pred_id_probas(IDX_CLASS+1, 0.0);
 
     for (unsigned int idx_id = 0; idx_id <= IDX_CLASS; idx_id++) {
-        if (pred_id_probas[idx_id] < multiclass_thresholds[idx_id]) {
-            pred_id_probas[idx_id] = 0.0;
+        auto pred_proba = output[ielem*NUM_OUTPUT_FEATURES + idx_id];
+        if (pred_proba < static_cast<float>(multiclass_thresholds_[idx_id])) {
+            pred_proba = 0.0;
         }
+        assert(!std::isnan(pred_proba));
+        pred_id_probas[idx_id] = pred_proba;
     }
-
-    std::cout << "pred_id: ";
-    for (auto v : pred_id_probas) {
-      std::cout << v << " ";
-    }
-    std::cout << std::endl;
 
     auto imax = argMax(pred_id_probas);
 
     //get the most probable class PDGID
     int pred_pid = pdgid_encoding[imax];
 
-    // if the charged candidate is generated from a track linked to a displaced vertex, we get
-    // JetTracksAssociatorExplicit/'ak4JetTracksAssociatorExplicitAll' -> Ref is inconsistent with RefVectorid
-    const reco::PFBlockElement* elem = selected_elements[ielem];
+    //a particle was predicted for this PFElement, otherwise it was a spectator
+    if (pred_pid != 0) {
 
-    //at the moment, I don't know what kind of specific configuration of refs & other metadata the muons expect downstream of PF
-    //so I currently reconstruct them as charged hadrons as a workaround to avoid crashes downstream.
-    //this needs debugging of the downstream reco modules to understand what defined a PFCandidate muon, besides the 4-momentum
-    if (pred_pid == 13) {
-      pred_pid = 211;
-    }
+      // if the charged candidate is generated from a track linked to a displaced vertex, we get
+      // JetTracksAssociatorExplicit/'ak4JetTracksAssociatorExplicitAll' -> Ref is inconsistent with RefVectorid
+      const reco::PFBlockElement* elem = selected_elements[ielem];
 
-    //muons and charged hadrons should only come from tracks, otherwise we won't have references to pass downstream
-    if (((pred_pid == 13) || (pred_pid==211)) && elem->type()!=reco::PFBlockElement::TRACK) {
-      pred_pid = 130;
-    }
-
-    if (elem->type()==reco::PFBlockElement::TRACK) {
-      const auto* eltTrack = dynamic_cast<const reco::PFBlockElementTrack*>(elem);
-
-      //a track with no muon ref should not produce a muon candidate, instead we interpret it as a charged hadron
-      if (pred_pid == 13 && eltTrack->muonRef().isNull()) {
+      //at the moment, I don't know what kind of specific configuration of refs & other metadata the muons expect downstream of PF
+      //so I currently reconstruct them as charged hadrons as a workaround to avoid crashes downstream.
+      //this needs debugging of the downstream reco modules to understand what defines a PFCandidate muon besides the 4-momentum
+      if (pred_pid == 13) {
         pred_pid = 211;
       }
 
-      //tracks from displaced vertices need reference debugging downstream as well, so we just treat them as neutrals for the moment
-      if ((pred_pid==211) && (eltTrack->isLinkedToDisplacedVertex())) {
+      //muons and charged hadrons should only come from tracks, otherwise we won't have references to pass downstream
+      if (((pred_pid == 13) || (pred_pid==211)) && elem->type()!=reco::PFBlockElement::TRACK) {
         pred_pid = 130;
       }
-    }
 
-    //get the predicted momentum components
-    float pred_pt = out_arr(0, ielem, IDX_PT);
-    float pred_eta = out_arr(0, ielem, IDX_ETA);
-    float pred_sin_phi = out_arr(0, ielem, IDX_SIN_PHI);
-    float pred_cos_phi = out_arr(0, ielem, IDX_COS_PHI);
-    float pred_e = out_arr(0, ielem, IDX_ENERGY);
-    float pred_charge = out_arr(0, ielem, IDX_CHARGE);
+      if (elem->type()==reco::PFBlockElement::TRACK) {
+        const auto* eltTrack = dynamic_cast<const reco::PFBlockElementTrack*>(elem);
 
-    //a particle was predicted for this PFElement, otherwise it was a spectator
-    if (pred_pid != 0) {
+        //a track with no muon ref should not produce a muon candidate, instead we interpret it as a charged hadron
+        if (pred_pid == 13 && eltTrack->muonRef().isNull()) {
+          pred_pid = 211;
+        }
+
+        //tracks from displaced vertices need reference debugging downstream as well, so we just treat them as neutrals for the moment
+        if ((pred_pid==211) && (eltTrack->isLinkedToDisplacedVertex())) {
+          pred_pid = 130;
+        }
+      }
+
+      //get the predicted momentum components
+      float pred_pt = output[ielem*NUM_OUTPUT_FEATURES + IDX_PT];
+      float pred_eta = output[ielem*NUM_OUTPUT_FEATURES + IDX_ETA];
+      float pred_sin_phi = output[ielem*NUM_OUTPUT_FEATURES + IDX_SIN_PHI];
+      float pred_cos_phi = output[ielem*NUM_OUTPUT_FEATURES + IDX_COS_PHI];
+      float pred_e = output[ielem*NUM_OUTPUT_FEATURES + IDX_ENERGY];
+      float pred_charge = output[ielem*NUM_OUTPUT_FEATURES + IDX_CHARGE];
 
       std::cout << "type=" << elem->type() << " pid=" << pred_pid << " "
           << pred_charge << " " << pred_pt << " " << pred_eta << " "
@@ -203,26 +156,18 @@ void MLPFProducer::produce(edm::Event& event, const edm::EventSetup& setup) {
   event.emplace(pfCandidatesPutToken_, pOutputCandidateCollection);
 }
 
-std::unique_ptr<MLPFCache> MLPFProducer::initializeGlobalCache(const edm::ParameterSet& params) {
-  // this method is supposed to create, initialize and return a MLPFCache instance
-  std::unique_ptr<MLPFCache> cache = std::make_unique<MLPFCache>();
-
-  //load the frozen TF graph of the GNN model
-  std::string path = params.getParameter<std::string>("model_path");
-  auto fullPath = edm::FileInPath(path).fullPath();
-  LogDebug("MLPFProducer") << "Initializing MLPF model from " << fullPath;
-
-  cache->graph_def = tensorflow::loadGraphDef(fullPath);
-
-  return cache;
+std::unique_ptr<ONNXRuntime> MLPFProducer::initializeGlobalCache(const edm::ParameterSet& params) {
+  return std::make_unique<ONNXRuntime>(params.getParameter<edm::FileInPath>("model_path").fullPath());
 }
 
-void MLPFProducer::globalEndJob(MLPFCache* cache) { delete cache->graph_def; }
+void MLPFProducer::globalEndJob(const ONNXRuntime* cache) {}
 
 void MLPFProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("src", edm::InputTag("particleFlowBlock"));
-  desc.add<std::string>("model_path", "RecoParticleFlow/PFProducer/data/mlpf/mlpf_2021_04_12.pb");
+  desc.add<edm::FileInPath>("model_path", edm::FileInPath("RecoParticleFlow/PFProducer/data/mlpf/mlpf_2021_06_28.onnx"));
+  const std::vector<double> thresholds({0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0});
+  desc.add<std::vector<double>>("multiclass_thresholds", thresholds);
   descriptions.addWithDefaultLabel(desc);
 }
 
