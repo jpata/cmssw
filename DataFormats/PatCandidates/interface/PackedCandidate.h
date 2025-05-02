fe4d9d353d40bd405f2cd61de670eb3ccf395f75
Ka Wa Ho <ka.wa.ho@cern.ch>
2025-05-02 19:44:25 +0200

    propagate mlpf pu prediction to btv nano

--- DataFormats/PatCandidates/interface/PackedCandidate.h
+++ DataFormats/PatCandidates/interface/PackedCandidate.h
@@ -47,6 +47,7 @@ namespace pat {
           packedCovariance_(),
           packedPuppiweight_(0),
           packedPuppiweightNoLepDiff_(0),
+          packedMLPFPUweight_(0),
           rawCaloFraction_(0),
           rawHcalFraction_(0),
           caloFraction_(0),
@@ -77,6 +78,7 @@ namespace pat {
                              reco::VertexRef::key_type pvRefKey)
         : packedPuppiweight_(0),
           packedPuppiweightNoLepDiff_(0),
+	  packedMLPFPUweight_(0),
           rawCaloFraction_(0),
           rawHcalFraction_(0),
           caloFraction_(0),
@@ -115,6 +117,7 @@ namespace pat {
                              reco::VertexRef::key_type pvRefKey)
         : packedPuppiweight_(0),
           packedPuppiweightNoLepDiff_(0),
+	  packedMLPFPUweight_(0),
           rawCaloFraction_(0),
           rawHcalFraction_(0),
           caloFraction_(0),
@@ -153,6 +156,7 @@ namespace pat {
                              reco::VertexRef::key_type pvRefKey)
         : packedPuppiweight_(0),
           packedPuppiweightNoLepDiff_(0),
+	  packedMLPFPUweight_(0),
           rawCaloFraction_(0),
           rawHcalFraction_(0),
           caloFraction_(0),
@@ -194,6 +198,7 @@ namespace pat {
           packedCovariance_(iOther.packedCovariance_),
           packedPuppiweight_(iOther.packedPuppiweight_),
           packedPuppiweightNoLepDiff_(iOther.packedPuppiweightNoLepDiff_),
+          packedMLPFPUweight_(iOther.packedMLPFPUweight_),
           rawCaloFraction_(iOther.rawCaloFraction_),
           rawHcalFraction_(iOther.rawHcalFraction_),
           caloFraction_(iOther.caloFraction_),
@@ -236,6 +241,7 @@ namespace pat {
           packedCovariance_(iOther.packedCovariance_),
           packedPuppiweight_(iOther.packedPuppiweight_),
           packedPuppiweightNoLepDiff_(iOther.packedPuppiweightNoLepDiff_),
+          packedMLPFPUweight_(iOther.packedMLPFPUweight_),
           rawCaloFraction_(iOther.rawCaloFraction_),
           rawHcalFraction_(iOther.rawHcalFraction_),
           caloFraction_(iOther.caloFraction_),
@@ -280,6 +286,7 @@ namespace pat {
       packedCovariance_ = iOther.packedCovariance_;
       packedPuppiweight_ = iOther.packedPuppiweight_;
       packedPuppiweightNoLepDiff_ = iOther.packedPuppiweightNoLepDiff_;
+      packedMLPFPUweight_ = iOther.packedMLPFPUweight_,
       rawCaloFraction_ = iOther.rawCaloFraction_;
       rawHcalFraction_ = iOther.rawHcalFraction_;
       caloFraction_ = iOther.caloFraction_;
@@ -358,6 +365,7 @@ namespace pat {
       packedCovariance_ = iOther.packedCovariance_;
       packedPuppiweight_ = iOther.packedPuppiweight_;
       packedPuppiweightNoLepDiff_ = iOther.packedPuppiweightNoLepDiff_;
+      packedMLPFPUweight_ = iOther.packedMLPFPUweight_,
       rawCaloFraction_ = iOther.rawCaloFraction_;
       rawHcalFraction_ = iOther.rawHcalFraction_;
       caloFraction_ = iOther.caloFraction_;
@@ -929,6 +937,9 @@ namespace pat {
     float puppiWeight() const;                 /// Weight from full PUPPI
     float puppiWeightNoLep() const;            /// Weight from PUPPI removing leptons
 
+    void setMLPFPUWeight(float p);  /// Set both weights at once (with
+                                               /// option for only full PUPPI)
+    float MLPFPUWeight() const;                 /// Weight from full PUPPI
     // for the neutral fractions
     void setRawCaloFraction(float p);  /// Set the raw ECAL+HCAL energy over candidate
                                        /// energy for isolated charged hadrons
@@ -1048,6 +1059,7 @@ namespace pat {
 
     uint8_t packedPuppiweight_;
     int8_t packedPuppiweightNoLepDiff_;  // storing the DIFFERENCE of (all - "no
+    uint8_t packedMLPFPUweight_;
                                          // lep") for compression optimization
     uint8_t rawCaloFraction_;
     int8_t rawHcalFraction_;
